463fe66fc15ed59ac70adcae891c6aa1eafde2d3
Siroson Mathuranga Sivarajah <siromathu@gmail.com>
2018-03-02 18:28:09 +0530

    Added new images assets

---
+++ SocialApp/Utilities/Configurations.swift
@@ -0,0 +1,9 @@
+//
+//  Configurations.swift
+//  SocialApp
+//
+//  Created by Siroson Mathuranga Sivarajah on 02/03/18.
+//  Copyright © 2018 Siroson Mathuranga Sivarajah. All rights reserved.
+//
+
+import Foundation
